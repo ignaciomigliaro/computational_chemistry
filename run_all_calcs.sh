ef1ae6d959ca41b5ba48ba6ea6f6b1c6f26737b8
ignaciomigliaro <ignaciomigliaro@outlook.com>
2024-03-12 17:59:16 -0600

    Update run_all_calcs.sh
    Now runs what I actually want, to sleep when jobs are pending.

--- run_all_calcs.sh
+++ run_all_calcs.sh
@@ -1,37 +1,25 @@
 #!/bin/bash
 
-# Function to run vasp command in each subdirectory
-run_vasp() {
+# Function to check if there are pending jobs in the queue
+check_pending_jobs() {
+    pending_jobs=$(squeue -u im0225 -p short.36 | awk '$5 == "PD" {print}' | wc -l)
+    echo "$pending_jobs"
+}
+
     for dir in */; do
         # Check if OUTCAR file exists and if 'Voluntary' pattern exists in the OUTCAR file
         if [[ -f "${dir}OUTCAR" && $(grep -c 'Voluntary' "${dir}OUTCAR") -gt 0 ]]; then
             echo "Skipping directory $dir because 'Voluntary' pattern found in OUTCAR."
             continue
         fi
-
         cd "$dir" || continue  # Move into the directory
         echo "Running vasp in directory: $dir"
-        vasp -b std -p short.36 -c 36 -m 16gb
-        sleep 20
+        vasp -b std -p short.36 -c 36 -m 50gb -s network
         cd ..  # Move back to the parent directory
-
-        # Check job status
-        job_status=$(squeue -u im0225 -p share.36 | awk -v d="$dir" 'NR>1 && $9 == d {print $5}')
-
-        # If there are any jobs in the "Running" state (R), continue to the next directory
-        while [[ "$job_status" == *"R"* ]]; do
-            echo "Jobs are still running in $dir. Waiting..."
-            sleep 10  # Check job status every 10 seconds
-            job_status=$(squeue -u im0225 -p share.36 | awk -v d="$dir" 'NR>1 && $9 == d {print $5}')
-        done
-
-        # If there are any jobs in the "Pending" state (PD), sleep for 60 seconds
-        if [[ "$job_status" == *"PD"* ]]; then
-            echo "There are pending jobs in $dir. Sleeping..."
-            sleep 60
-        fi
+        pending_jobs=$(check_pending_jobs)
+        echo "There are $pending_jobs in Queue"
+        while [[ $(check_pending_jobs) -gt 0 ]]; do
+                echo "There are pending jobs in the queue. Sleeping..."
+                sleep 300
     done
-}
-
-# Run vasp in each subdirectory and check job status
-run_vasp
+done
